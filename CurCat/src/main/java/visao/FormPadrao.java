cc7347e89bf609e6c96d91e8c68045f6e5246564
Esther <alvesrezendeesther777@gmail.com>
2023-12-03 13:23:01 -0300

    alterações básicas

--- CurCat/src/main/java/visao/FormPadrao.java
+++ CurCat/src/main/java/visao/FormPadrao.java
@@ -26,21 +26,130 @@ public class FormPadrao extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jpnBotoes = new javax.swing.JPanel();
+        jbNovo = new javax.swing.JButton();
+        jbAlterar = new javax.swing.JButton();
+        jbExcluir = new javax.swing.JButton();
+        jbSalvar = new javax.swing.JButton();
+        jbCancelar = new javax.swing.JButton();
+        jbFechar = new javax.swing.JButton();
+        jpnFormulario = new javax.swing.JPanel();
+        jpnConsulta = new javax.swing.JPanel();
+
+        jbNovo.setIcon(new javax.swing.ImageIcon("C:\\Users\\rezen\\Downloads\\img lp3\\134224_add_plus_new_icon.png")); // NOI18N
+        jbNovo.setText("Novo");
+
+        jbAlterar.setIcon(new javax.swing.ImageIcon("C:\\Users\\rezen\\Downloads\\img lp3\\alterar.png")); // NOI18N
+        jbAlterar.setText("Alterar");
+
+        jbExcluir.setIcon(new javax.swing.ImageIcon("C:\\Users\\rezen\\Downloads\\img lp3\\lixo.png")); // NOI18N
+        jbExcluir.setText("Excluir");
+
+        jbSalvar.setIcon(new javax.swing.ImageIcon("C:\\Users\\rezen\\Downloads\\img lp3\\salvar.png")); // NOI18N
+        jbSalvar.setText("Salvar");
+
+        jbCancelar.setIcon(new javax.swing.ImageIcon("C:\\Users\\rezen\\Downloads\\img lp3\\cancelar.png")); // NOI18N
+        jbCancelar.setText("Cancelar");
+
+        jbFechar.setIcon(new javax.swing.ImageIcon("C:\\Users\\rezen\\Downloads\\img lp3\\fechar.png")); // NOI18N
+        jbFechar.setText("Fechar");
+        jbFechar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbFecharActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jpnBotoesLayout = new javax.swing.GroupLayout(jpnBotoes);
+        jpnBotoes.setLayout(jpnBotoesLayout);
+        jpnBotoesLayout.setHorizontalGroup(
+            jpnBotoesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jpnBotoesLayout.createSequentialGroup()
+                .addComponent(jbNovo)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jbAlterar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jbExcluir)
+                .addGap(50, 50, 50)
+                .addComponent(jbSalvar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jbCancelar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 29, Short.MAX_VALUE)
+                .addComponent(jbFechar)
+                .addGap(20, 20, 20))
+        );
+        jpnBotoesLayout.setVerticalGroup(
+            jpnBotoesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jpnBotoesLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jpnBotoesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jbNovo)
+                    .addComponent(jbAlterar)
+                    .addComponent(jbExcluir)
+                    .addComponent(jbSalvar)
+                    .addComponent(jbCancelar)
+                    .addComponent(jbFechar))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout jpnFormularioLayout = new javax.swing.GroupLayout(jpnFormulario);
+        jpnFormulario.setLayout(jpnFormularioLayout);
+        jpnFormularioLayout.setHorizontalGroup(
+            jpnFormularioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        jpnFormularioLayout.setVerticalGroup(
+            jpnFormularioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 139, Short.MAX_VALUE)
+        );
+
+        jpnConsulta.setBackground(new java.awt.Color(153, 204, 255));
+
+        javax.swing.GroupLayout jpnConsultaLayout = new javax.swing.GroupLayout(jpnConsulta);
+        jpnConsulta.setLayout(jpnConsultaLayout);
+        jpnConsultaLayout.setHorizontalGroup(
+            jpnConsultaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        jpnConsultaLayout.setVerticalGroup(
+            jpnConsultaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 268, Short.MAX_VALUE)
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
+            .addComponent(jpnFormulario, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jpnConsulta, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jpnBotoes, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 274, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jpnBotoes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jpnFormulario, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jpnConsulta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
-        pack();
+        setBounds(0, 0, 688, 504);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jbFecharActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbFecharActionPerformed
+        dispose();
+    }//GEN-LAST:event_jbFecharActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jbAlterar;
+    private javax.swing.JButton jbCancelar;
+    private javax.swing.JButton jbExcluir;
+    private javax.swing.JButton jbFechar;
+    private javax.swing.JButton jbNovo;
+    private javax.swing.JButton jbSalvar;
+    private javax.swing.JPanel jpnBotoes;
+    private javax.swing.JPanel jpnConsulta;
+    private javax.swing.JPanel jpnFormulario;
     // End of variables declaration//GEN-END:variables
 }
