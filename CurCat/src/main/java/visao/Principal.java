531a1559cd768a23f73e7c394c52e08b8168014c
Esther <alvesrezendeesther777@gmail.com>
2023-12-03 12:14:49 -0300

    Criada a tela principal do projeto

---
+++ CurCat/src/main/java/visao/Principal.java
@@ -0,0 +1,51 @@
+
+package visao;
+
+import javax.swing.JFrame;
+
+
+public class Principal extends javax.swing.JFrame {
+
+    
+    public Principal() {
+        initComponents();
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("CurCat - Catálogo de Cursos");
+        setResizable(false);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 800, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 600, Short.MAX_VALUE)
+        );
+
+        setSize(new java.awt.Dimension(816, 608));
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    
+    public static void main(String args[]) {
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Principal().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
